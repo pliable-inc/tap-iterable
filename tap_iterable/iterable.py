55e582f88858377831bf7288b5e2fec536b48c2b
Matt Sullivan <mattotodd@gmail.com>
2025-11-17 13:15:02 -0500

    fix: indentation

--- tap_iterable/iterable.py
+++ tap_iterable/iterable.py
@@ -30,10 +30,10 @@ class Iterable(object):
         self.MAX_BYTES = 10240
         self.CHUNK_SIZE = 512
         self.headers = {"Api-Key": self.api_key}
-        
+    
     def _now(self):
         return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        
+    
     def _daterange(self, start_date, end_date):
         total_days = (utils.strptime_with_tz(end_date) - utils.strptime_with_tz(start_date)).days
         if total_days >= self.api_window_in_days:
@@ -41,14 +41,14 @@ class Iterable(object):
                 yield (utils.strptime_with_tz(start_date) + n * timedelta(int(self.api_window_in_days))).strftime("%Y-%m-%d %H:%M:%S")
         else:
             yield start_date
-            
+        
     def _get_end_datetime(self, startDateTime):
         endDateTime = utils.strptime_with_tz(startDateTime) + timedelta(self.api_window_in_days)
         return endDateTime.strftime("%Y-%m-%d %H:%M:%S")
-        
+    
     def retry_handler(details):
         logger.info("Received 429 -- sleeping for %s seconds", details['wait'])
-    
+
     @staticmethod
     def connection_retry_handler(func):
         """Decorator to retry on connection failures with exponential backoff."""
@@ -148,102 +148,100 @@ class Iterable(object):
         
         return session
     
-  #
-  # The common `get` request.
-  #
+
   
-  def get(self, path, **kwargs):
-    response = self._get(path, **kwargs)
-    return response.json()
+    def get(self, path, **kwargs):
+        response = self._get(path, **kwargs)
+        return response.json()
 
   #
   # Get custom user fields, used for generating `users` schema in `discover`.
   #
 
-  def get_user_fields(self):
-    return self.get("users/getFields")
+    def get_user_fields(self):
+        return self.get("users/getFields")
 
   # 
   # Methods to retrieve data per stream/resource.
   # 
 
-  def lists(self, column_name=None, bookmark=None):
-    res = self.get("lists")
-    for l in res["lists"]:
-      yield l
+    def lists(self, column_name=None, bookmark=None):
+        res = self.get("lists")
+        for l in res["lists"]:
+          yield l
 
 
-  def list_users(self, column_name=None, bookmark=None):
-    res = self.get("lists")
-    for l in res["lists"]:
-      kwargs = {
-        "listId": l["id"]
-      }
-      users = self._get("lists/getUsers", **kwargs)
-      for user in users:
-        yield {
-          "email": user,
-          "listId": l["id"],
-          "updatedAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S %Z")
-        }
+    def list_users(self, column_name=None, bookmark=None):
+        res = self.get("lists")
+        for l in res["lists"]:
+          kwargs = {
+            "listId": l["id"]
+          }
+          users = self._get("lists/getUsers", **kwargs)
+          for user in users:
+            yield {
+              "email": user,
+              "listId": l["id"],
+              "updatedAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S %Z")
+            }
 
 
-  def campaigns(self, column_name=None, bookmark=None):
-    res = self.get("campaigns")
-    for c in res["campaigns"]:
-      yield c
+    def campaigns(self, column_name=None, bookmark=None):
+        res = self.get("campaigns")
+        for c in res["campaigns"]:
+          yield c
 
 
-  def channels(self, column_name, bookmark):
-    res = self.get("channels")
-    for c in res["channels"]:
-      yield c
+    def channels(self, column_name, bookmark):
+        res = self.get("channels")
+        for c in res["channels"]:
+          yield c
 
 
-  def message_types(self, column_name=None, bookmark=None):
-    res = self.get("messageTypes")
-    for m in res["messageTypes"]:
-      yield m
+    def message_types(self, column_name=None, bookmark=None):
+        res = self.get("messageTypes")
+        for m in res["messageTypes"]:
+          yield m
 
 
-  def templates(self, column_name, bookmark):
-    template_types = [
-      "Base",
-      "Blast",
-      "Triggered",
-      "Workflow"
-    ]
-    message_mediums = [
-      "Email",
-      "Push",
-      "InApp",
-      "SMS"
-    ]
-    for template_type in template_types:
-      for medium in message_mediums:
-        res = self.get("templates", templateTypes=template_type, messageMedium=medium)
-        for t in res["templates"]:
-          yield t
+    def templates(self, column_name, bookmark):
+        template_types = [
+          "Base",
+          "Blast",
+          "Triggered",
+          "Workflow"
+        ]
+        message_mediums = [
+          "Email",
+          "Push",
+          "InApp",
+          "SMS"
+        ]
+        for template_type in template_types:
+          for medium in message_mediums:
+            res = self.get("templates", templateTypes=template_type, messageMedium=medium)
+            for t in res["templates"]:
+              yield t
 
 
-  def metadata(self, column_name=None, bookmark=None):
-    tables = self.get("metadata")
-    for t in tables["results"]:
-      keys = self.get("metadata/{table_name}".format(table_name=t["name"]))
-      for k in keys["results"]:
-        value = self.get("metadata/{table_name}/{key}".format(table_name=k["table"], key=k["key"]))
-        yield value
+    def metadata(self, column_name=None, bookmark=None):
+        tables = self.get("metadata")
+        for t in tables["results"]:
+          keys = self.get("metadata/{table_name}".format(table_name=t["name"]))
+          for k in keys["results"]:
+            value = self.get("metadata/{table_name}/{key}".format(table_name=k["table"], key=k["key"]))
+            yield value
 
 
-  def get_data_export_generator(self, data_type_name, bookmark=None):
-    now = self._now()
-    kwargs = {}
-    for start_date_time in self._daterange(bookmark, now):
-      kwargs["startDateTime"] = start_date_time
-      kwargs["endDateTime"] = self._get_end_datetime(startDateTime=start_date_time)
-      def get_data():
-        return self._get("export/data.json", dataTypeName=data_type_name, **kwargs)
-      yield get_data
+    def get_data_export_generator(self, data_type_name, bookmark=None):
+        now = self._now()
+        kwargs = {}
+        for start_date_time in self._daterange(bookmark, now):
+          kwargs["startDateTime"] = start_date_time
+          kwargs["endDateTime"] = self._get_end_datetime(startDateTime=start_date_time)
+          def get_data():
+            return self._get("export/data.json", dataTypeName=data_type_name, **kwargs)
+          yield get_data
 
 
 
